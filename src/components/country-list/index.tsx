73c7c9952a96c94848c8929a7ae713929119840a
binoycdarvin <83066688+binoycdarvin@users.noreply.github.com>
2025-03-05 22:40:37 +0530

    country list

--- src/components/country-list/index.tsx
+++ src/components/country-list/index.tsx
@@ -1,27 +1,23 @@
-import { useDispatch, useSelector } from "react-redux";
-import { CountryCard } from "../country-card";
-import "./country-list.scss";
-import { AppDispatch, RootState } from "../../store/store";
-import { useEffect } from "react";
+import { useDispatch, useSelector } from 'react-redux';
+import { CountryCard } from '../country-card';
+import { AppDispatch, RootState } from '../../store/store';
+import { useEffect } from 'react';
+import { fetchCountryList } from '../../services/country-list/country-list.service';
+import './country-list.scss';
 
 export const CountryList = () => {
-  const dispatch = useDispatch<AppDispatch>();
-  const countryList = useSelector((state: RootState) => state.countryList);
+	const dispatch = useDispatch<AppDispatch>();
+	const countryList = useSelector((state: RootState) => state.countryList);
 
-  useEffect(() => {
-    // dispatch(fetchCountryList());
-  }, []);
+	useEffect(() => {
+		dispatch(fetchCountryList());
+	}, []);
 
-  return (
-    <div className="country-list-container">
-      {countryList.data.map((country) => (
-        <CountryCard
-          key={country.name}
-          flag={country.flag}
-          country={country.name}
-          region={country.region}
-        />
-      ))}
-    </div>
-  );
+	return (
+		<div className='country-list-container'>
+			{countryList.data.map((country) => (
+				<CountryCard key={country.name} flag={country.flag} country={country.name} region={country.region} />
+			))}
+		</div>
+	);
 };
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,35 +1,12 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { AppDispatch, RootState } from '../../store/store';
-import { ChangeEvent, useEffect, useState } from 'react';
-import { fetchCountryList } from '../../services/country-list/country-list.service';
 import { CountryCarousel } from '../../components/country-carousel';
 import { CountryList } from '../../components/country-list';
 import { Navigation } from '../../components/navigation';
 import { Col, Container, Row, Image } from 'react-bootstrap';
 import { Header } from '../../components/header';
-import './home.scss';
 import { Footer } from '../../components/footer';
+import './home.scss';
 
 export const Home = () => {
-	const dispatch = useDispatch<AppDispatch>();
-	const [selectedOption, setSelectedOption] = useState('');
-
-	const countryList = useSelector((state: RootState) => {
-		if (selectedOption !== '') {
-			const data = state.countryList.data.filter((d) => d.region === selectedOption);
-			return { ...state.countryList, data };
-		}
-		return state.countryList;
-	});
-
-	useEffect(() => {
-		dispatch(fetchCountryList());
-	}, []);
-
-	const handleTabChange = (e: ChangeEvent<HTMLInputElement>) => {
-		setSelectedOption(e.target.value);
-	};
-
 	return (
 		<div className='home-container'>
 			<Navigation />
@@ -51,6 +28,9 @@ export const Home = () => {
 							<Image src='https://placehold.co/600x400' />
 						</div>
 					</Col>
+				</Row>
+				<Row>
+					<CountryList />
 				</Row>
 				<Row>
 					<Footer />
