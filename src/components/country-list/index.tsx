9321c0f407e209080f2b2ad0a620dc0e6c83561c
binoycdarvin <83066688+binoycdarvin@users.noreply.github.com>
2025-03-05 23:50:08 +0530

    country list

--- src/components/country-list/index.tsx
+++ src/components/country-list/index.tsx
@@ -1,23 +1,47 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { CountryCard } from '../country-card';
 import { AppDispatch, RootState } from '../../store/store';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { fetchCountryList } from '../../services/country-list/country-list.service';
+import { Button } from 'react-bootstrap';
+import { ApiStatus } from '../../types/types';
+import { Loader } from '../loader';
 import './country-list.scss';
 
 export const CountryList = () => {
 	const dispatch = useDispatch<AppDispatch>();
 	const countryList = useSelector((state: RootState) => state.countryList);
 
+	const [index, setIndex] = useState(1);
+
 	useEffect(() => {
 		dispatch(fetchCountryList());
 	}, []);
 
+	const handleLoadMore = () => {
+		if (index * 10 < countryList.data.length) {
+			setIndex((prev) => prev + 1);
+		}
+	};
+
 	return (
-		<div className='country-list-container'>
-			{countryList.data.map((country) => (
-				<CountryCard key={country.name} flag={country.flag} country={country.name} region={country.region} />
-			))}
-		</div>
+		<>
+			{(countryList.status === ApiStatus.Pending || countryList.status === ApiStatus.Rejected) && (
+				<Loader status={countryList.status} />
+			)}
+
+			{countryList.status === ApiStatus.Fulfilled && (
+				<>
+					<div className='country-list-container'>
+						{countryList.data.slice(0, index * 10).map((country) => (
+							<CountryCard key={country.name} flag={country.flag} country={country.name} region={country.region} />
+						))}
+					</div>
+					<Button variant='outline-secondary' className='mt-5' onClick={handleLoadMore}>
+						Load More
+					</Button>
+				</>
+			)}
+		</>
 	);
 };
